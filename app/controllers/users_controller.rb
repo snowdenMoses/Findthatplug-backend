19dc536c532820b9d71f1853cc0c0acbcb50d1ab
AbubakarMoses <111445710+AbubakarMoses@users.noreply.github.com>
2023-02-01 12:17:47 +0100

    add user model and controller's actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,29 @@
 class UsersController < ApplicationController
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def delete
+
+  end
+
+  def destroy
+
+  end
 end
