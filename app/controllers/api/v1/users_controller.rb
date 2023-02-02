8ae43b3a1ed33b9260ba784dff57adaa7d9cb44d
snowdenmoses <111445710+AbubakarMoses@users.noreply.github.com>
2023-02-02 23:56:37 +0100

    update authorization

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,7 @@
 module Api
   module V1
     class UsersController < ApplicationController
+      # skip_before_action :authorize, only:[:index]
       def index
         users = User.all
         render json: {data: users}, status: :ok
