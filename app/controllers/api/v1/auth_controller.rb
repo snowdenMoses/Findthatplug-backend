3fc30789e3cefa35e5891c10c29bc0ddf0cc57d9
snowdenmoses <111445710+AbubakarMoses@users.noreply.github.com>
2023-02-04 03:46:06 +0100

    update application controller

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::AuthController < ApplicationController
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       token = JWT.encode({user_id: user.id}, Rails.application.secrets.secret_key_base)
-      render json: {token: token}, status: :ok
+      render json: {token: token, user_id: user.id}, status: :ok
     else
       render json: {error: 'Invalid credentials'}, status: :unauthorized
     end
