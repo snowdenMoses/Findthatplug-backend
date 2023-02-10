b65bf3511485cc3a74c602a954799da17448d98f
snowdenmoses <111445710+AbubakarMoses@users.noreply.github.com>
2023-02-10 01:32:17 +0100

    creates products with categories

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,21 +1,24 @@
 class Api::V1::ProductsController < ApplicationController
   def index
-    products = Product.all
+    products = Product.includes(:categories).all
     render json: products
-  end
-
+    end
   def show
-
+    product = Product.find_by(id: params[:id])
+    render json: {data: product, prod_cat: product.categories}
   end
-def create
-  product = Product.new(payload)
-  product.user = current_user
+  def create
+    product = Product.new(payload)
+    # mappedArray = params[:categories].map { |num| num }
+    # category = Category.find_by(id: params[:categories])
+    product.categories << params[:categories]
+    product.user = current_user
 
-  if product.save
-    render json: product, status: :ok
-  else
-    render json: {data: product.errors, message: "Product not created"}, status: :unprocessable_entity
-  end
+    if product.save
+      render json: {data: product, message: "Product Successfully Added"}, status: :ok
+    else
+      render json: {data: product.errors, message: "Product not created"}, status: :unprocessable_entity
+    end
 end
 
 private
